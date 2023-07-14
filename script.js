ae3c17d66c8955441277ba8ddf01f4fc3ba0ad57
Juaokkkk <117794205+Juaokkkk@users.noreply.github.com>
2023-07-14 00:29:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const pointerH = document.querySelector('.hour');
+const pointerM = document.querySelector('.minute');
+const pointerS = document.querySelector('.second');
+
+const getTime = () => {
+    const date = new Date();
+    
+    return{
+     hours:  date.getHours(),
+     minutes: date.getMinutes(),
+     seconds: date.getSeconds(),
+    };
+}
+
+setInterval(() => {
+
+    const { seconds, minutes, hours } = getTime();
+
+    pointerS.style.transform = `translateY(-50%) rotate(${seconds*6}deg)`;
+    pointerM.style.transform = `translateY(-50%) rotate(${minutes*6}deg)`;
+    pointerH.style.transform = `translateY(-50%) rotate(${hours*30}deg)`;
+
+}, 1000); 
