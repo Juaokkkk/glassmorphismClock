31027cd556ecd80f7eb526629b9fdc59cac05a6a
Juaokkkk <117794205+Juaokkkk@users.noreply.github.com>
2023-08-18 20:51:51 -0400

    Atualizar o script.js
    melhorias do código

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-const pointerH = document.querySelector('.hour');
+const pointerH =  document.querySelector('.hour');
 const pointerM = document.querySelector('.minute');
 const pointerS = document.querySelector('.second');
 
-const getTime = () => {
+const getTime = () =>{
     const date = new Date();
-    
+
     return{
-     hours:  date.getHours(),
-     minutes: date.getMinutes(),
-     seconds: date.getSeconds(),
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
     };
 }
 
-setInterval(() => {
-
-    const { seconds, minutes, hours } = getTime();
-
+function update() {
+    const { seconds,minutes,hours} = getTime();
+    
     pointerS.style.transform = `translateY(-50%) rotate(${seconds*6}deg)`;
     pointerM.style.transform = `translateY(-50%) rotate(${minutes*6}deg)`;
     pointerH.style.transform = `translateY(-50%) rotate(${hours*30}deg)`;
+}
 
-}, 1000); 
+update();
+setInterval(update, 1000);
